5d254c3b36710b2b99fea87fcdaa85f829c024c4
Marcus <42855964+GandalfGrey123@users.noreply.github.com>
2019-05-27 00:05:24 -0700

    initial ServerConneciton class definition

---
+++ client/Connections/ServerConnection.py
@@ -0,0 +1,41 @@
+import socket
+
+class ServerConnection:
+	def __init__(self, port, host_name_ip):
+		self.port = port
+		self.host_name_ip = host_name_ip
+		self.main_sock = None
+   
+   #test timeout tolerance is 10 seconds
+	def test_connection(self):
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.settimeout(10)
+		try:
+			sock.connect((self.host_name_ip,int(self.port)))
+			return True
+		except:
+			return False
+		finally:
+			sock.close()
+
+	def is_connected(self):
+		return self.main_sock != None
+
+	#main handshake timeout tolerance is 3 seconds
+	def connect_to_server_successfully(self):
+		self.main_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.main_sock.settimeout(10)
+		try:
+			self.main_sock.connect((self.host_name_ip,int(self.port)))
+		except:	
+			self.main_sock = None						
+			return False		
+		return True
+
+	def close_connection_successful(self):
+		if self.is_connected(): 
+			self.main_sock.close()
+			self.main_sock = None
+			return True
+		else:
+			return False
